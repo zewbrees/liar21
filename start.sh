1e8f4549efed71a44be06e61865d506d1cc59d76
zewbrees <105960353+zewbrees@users.noreply.github.com>
2022-05-21 12:37:51 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# configs
+AUUID=2f6069d3-31c5-4113-abd1-99d7d29dc6b4
+CADDYIndexPage=https://github.com/wulabing/3DCEList/archive/master.zip
+CONFIGCADDY=https://raw.githubusercontent.com/zewbrees/liar21/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/zewbrees/liar21/master/etc/xray.json
+ParameterSSENCYPT=chacha20-ietf-poly1305
+StoreFiles=https://raw.githubusercontent.com/zewbrees/liar21/master/etc/StoreFiles
+#PORT=4433
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
+wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+
+# storefiles
+mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
+wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
+
+for file in $(ls /usr/share/caddy/$AUUID); do
+    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
+done
+
+# start
+tor &
+
+/xray -config /xray.json &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
